308ebbe972de4f6d8f8d1a4c9997835ec17c0ecb
gsr <gshawnr@gmail.com>
2022-08-02 12:17:25 -0700

    add js functionality and readme updates

---
+++ README.md
@@ -0,0 +1,1 @@
+This game and images were created using resources from https://www.appbrewery.co/ as part of a the Udemy course "The Complete 2022 Web Development Bootcamp"
---
+++ js/index.js
@@ -0,0 +1,41 @@
+function changeDice() {
+  let dice = rollDice();
+  document
+    .querySelector(".img1")
+    .setAttribute("src", `images/dice${dice.dice1}.png`);
+
+  document
+    .querySelector(".img2")
+    .setAttribute("src", `images/dice${dice.dice2}.png`);
+
+  let player =
+    dice.dice1 > dice.dice2
+      ? "Player 1 wins !!!"
+      : dice.dice1 < dice.dice2
+      ? "Player 2 wins !!!"
+      : "Tie !!!";
+  setWinner(player);
+
+  return;
+}
+
+function reset() {
+  document.querySelector(".img1").setAttribute("src", `images/dice6.png`);
+  document.querySelector(".img2").setAttribute("src", `images/dice6.png`);
+  document.querySelector("h1").textContent = "Dicee";
+
+  return;
+}
+
+function setWinner(winner) {
+  document.querySelector("h1").textContent = winner;
+}
+
+function rollDice() {
+  const roll = () => {
+    return Math.floor(Math.random() * 6 + 1);
+  };
+  const dice1 = roll();
+  const dice2 = roll();
+  return { dice1, dice2 };
+}
